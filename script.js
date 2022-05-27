e7f9bfccea4594783c1cd4108bba23275b34130e
Lena Herrmann <herrmann-lena@t-online.de>
2022-05-27 20:51:31 +0200

    implemented functionality to get random jokes from the API

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 const joke = document.getElementById('joke'); 
 const newJokeBtn = document.getElementById('new-joke-btn'); 
+
+getRandomJoke();
+
+async function getRandomJoke() {
+    const config = { headers: { 'Accept': 'application/json' } }; 
+
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+    const data = await res.json();
+    joke.innerHTML = data.joke; 
+}
+
+newJokeBtn.addEventListener('click', getRandomJoke); 
